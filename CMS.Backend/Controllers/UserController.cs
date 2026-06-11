cfd10bb4301e46325ad088ec3240be8fd1e455a1
truong-2005 <163482327+truong-2005@users.noreply.github.com>
2026-06-11 08:34:12 +0700

    Hoan thanh buoi 1

---
+++ CMS.Backend/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CMS.Data.Entities; // Phải có dòng này để dùng lớp User
+
+namespace CMS.Backend.Controllers
+{
+    public class UserController : Controller
+    {
+        // Hàm Index: Hiển thị danh sách thành viên quản trị
+        public IActionResult Index()
+        {
+            // 1. Tạo danh sách Người dùng giả (Mock Data)
+            var users = new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    Username = "admin_thai",
+                    FullName = "Nguyễn Cao Thái",
+                    Role = "Administrator"
+                },
+                new User
+                {
+                    Id = 2,
+                    Username = "editor_01",
+                    FullName = "Trần Văn Biên Tập",
+                    Role = "Editor"
+                },
+                new User
+                {
+                    Id = 3,
+                    Username = "author_minh",
+                    FullName = "Lê Quang Minh",
+                    Role = "Author"
+                }
+            };
+
+            // 2. Trả về View kèm theo danh sách người dùng
+            return View(users);
+        }
+    }
+}
